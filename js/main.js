712b73fdeb747951e91265a9630f36607e8064c6
Floriane Siedlecki <florianesiedlecki@gmail.com>
2021-08-27 16:17:08 +0200

    update

--- js/main.js
+++ js/main.js
@@ -1,31 +1,31 @@
 /*---navigation menu-----*/
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
-/*-----------about section tabs----------*/
-(() =>{
-    const aboutSection = document.querySelector(".about-section"),
-    tabsContainer = document.querySelector(".about-tabs");
+}
 
-    tabsContainer.addEventListener("click", (event) =>{
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+/*-----------about section tabs----------*/
+(() => {
+    const aboutSection = document.querySelector(".about-section"),
+        tabsContainer = document.querySelector(".about-tabs");
+
+    tabsContainer.addEventListener("click", (event) => {
         /* if event.target contains 'tab-item' class and not
         contains 'active class*/
-        if(event.target.classList.contains("tab-item") &&
-            !event.target.classList.contains("active")){
+        if (event.target.classList.contains("tab-item") &&
+            !event.target.classList.contains("active")) {
             const target = event.target.getAttribute("data-target");
             //desactivate existing active 'tab-item'
             tabsContainer.querySelector(".active").classList.remove("outer-shadow"
-            ,"active");
+                , "active");
             // activate new 'tab-item'
-            event.target.classList.add("active","outer-shadow");
+            event.target.classList.add("active", "outer-shadow");
             //desactivate existing active 'tab-content'
             aboutSection.querySelector(".tab-content.active").classList.remove("active");
             // activate new 'tab-content'
-            aboutSection.querySelector(target).classList.add("active"); 
+            aboutSection.querySelector(target).classList.add("active");
         }
     })
 })();
